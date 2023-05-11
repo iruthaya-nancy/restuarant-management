f50d0e05de9bf488be6593088f1b4c17eefa25f2
Nancy <iruthaya05@gmail.com>
2023-05-11 13:44:58 +0530

    backend-new

--- src/main/java/com/example/restaurant/util/Config.java
+++ src/main/java/com/example/restaurant/util/Config.java
@@ -7,22 +7,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-//@Configuration
-//public class Config {
-//	@Bean
-//    public JavaMailSender javaMailSender() {
-//		
-//		//JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-////        mailSender.setHost("smtp.gmail.com");
-////        mailSender.setPort(587);
-////        
-////        
-////        
-////        Properties props = mailSender.getJavaMailProperties();
-////        props.put("mail.transport.protocol", "smtp");
-////        props.put("mail.smtp.auth", "true");
-////        props.put("mail.smtp.starttls.enable", "true");
-////        props.put("mail.debug", "true");
-//        return new JavaMailSenderImpl();
-//    }
-//}
+@Configuration
+public class Config {
+	@Bean
+    public JavaMailSender javaMailSender() {
+		
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//        
+        return new JavaMailSenderImpl();
+    }
+}
